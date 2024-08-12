fd4776d84b4ac25726dab7a2320296fd7e96b566
duong <hvdvietnamquetoi@gmail.com>
2024-08-12 23:25:02 +0700

    add var

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -11,7 +11,7 @@ class LoginController extends Controller
      */
     public function index()
     {
-        //
+        $main = "hoangvanduong";
     }
 
     /**
@@ -19,7 +19,7 @@ class LoginController extends Controller
      */
     public function create()
     {
-        //
+        $main = "hoangvanduong";
     }
 
     /**
